7e5ad466b686ff14c2493850b10afb973293aa36
Alexis Santana Gonzalez <Alexis Santana Gonzalez>
2017-05-18 07:31:26 -0500

    Conexion a base de datos con pdo

--- Database_connect.php
+++ Database_connect.php
@@ -1,20 +1,38 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "1234";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+class Connection{
+
+    protected $db;
+
+    public function Connection(){
+
+    $conn = NULL;
+    $dsn = 'mysql:dbname=alexissg_grama;host=localhost';
+    $username = 'materialesgrama';
+    $password = 'Materiales-grama';
+
+        try{
+
+            $conn = new PDO($dsn, $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+
+            } catch(PDOException $e){
+                echo 'ERROR: ' . $e->getMessage();
+                }
+            $this->db = $conn;
     }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
+
+    public function getConnection(){
+        return $this->db;
     }
+}
 
+try{
+$conexion = new Connection();
+$db = $conexion->getConnection();
+}catch(PDOException  $e ){
+echo 'Error: '.$e;
+}
 
-
-
- ?>
+?>
