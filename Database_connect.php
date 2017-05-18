906918523640270398bc58bae1900eee335365fb
Alexis Santana Gonzalez <Alexis Santana Gonzalez>
2017-05-18 07:46:27 -0500

    Actualizacion de rama

---
+++ Database_connect.php
@@ -0,0 +1,38 @@
+<?php
+
+class Connection{
+
+    protected $db;
+
+    public function Connection(){
+
+    $conn = NULL;
+    $dsn = 'mysql:dbname=alexissg_grama;host=localhost';
+    $username = 'materialesgrama';
+    $password = 'Materiales-grama';
+
+        try{
+
+            $conn = new PDO($dsn, $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+
+            } catch(PDOException $e){
+                echo 'ERROR: ' . $e->getMessage();
+                }
+            $this->db = $conn;
+    }
+
+    public function getConnection(){
+        return $this->db;
+    }
+}
+
+try{
+$conexion = new Connection();
+$db = $conexion->getConnection();
+}catch(PDOException  $e ){
+echo 'Error: '.$e;
+}
+
+?>
