715eae5b0f0e4d8e61aebe9515bdf912721f9726
Alexis Santana Gonzalez <Alexis Santana Gonzalez>
2017-05-21 23:05:02 -0500

    MVC Productos

--- Controladores/ProductoCtrl.php
+++ Controladores/ProductoCtrl.php
@@ -12,15 +12,22 @@ class ProdcutosCtrl
 
   function ejecutar()
   {
-    if(!isset($_GET['act'])){
-      $_GET['act'] = 'listar';
-    }
-
     switch ($_GET['act']) {
       case 'listar':
         $this->listar();
         break;
-
+      case 'insertar':
+        $this->insertar();
+        break;
+      case 'modificar':
+        $this->modificar();
+        break;
+      case 'borrar':
+        $this->borrar();
+        break;
+      case 'listar_':
+        $this->listar_();
+        break;
       default:
         echo 'Accion no reconocida';
         break;
@@ -39,9 +46,76 @@ class ProdcutosCtrl
 
   function procesarVista($data)
   {
-     
+
   }
 
+  function insertar()
+  {
+
+    if(empty($_POST)){
+					require_once("Vista/AltaProductos.html");
+				}
+				else{
+          $id       =$_POST["id"];
+					$nombre 	= $_POST["nombre"];
+					$categoria 	= $_POST["categoria"];
+					$descripcion 	= $_POST["descripcion"];
+					$ruta_imagen 	= $_POST["ruta_imagen"];
+        }
+      $productos = $this->model->insertarProducto($nombre, $categoria, $descripcion, $ruta_imagen);
+
+
+  }
+
+  function modificar()
+  {
+    if(empty($_POST)){
+
+					require_once("Vista/Modificar_Productos.html");
+				}
+				else{
+
+          $id       =$_POST["id"];
+					$nombre 	= $_POST["nombre"];
+					$categoria 	= $_POST["categoria"];
+					$descripcion 	= $_POST["descripcion"];
+					$ruta_imagen 	= $_POST["ruta_imagen"];
+        }
+
+      $productos = $this->model->modificarProducto($id, $nombre, $categoria, $descripcion, $ruta_imagen);
+
+  }
+
+  function borrar()
+  {
+    if(empty($_POST)){
+
+          require_once("Vista/Eliminar_Productos.html");
+        }
+        else{
+
+          $id       =$_POST["id"];
+        }
+      $productos = $this->model->borrarProducto($id);
+
+  }
+  function listar_()
+  {
+    if(empty($_POST)){
+
+          require_once("Vista/Eliminar_Productos.html");
+        }
+        else{
+
+          $id       =$_POST["id"];
+        }
+      $productos = $this->model->obtenerProducto_($id);
+
+      if(isset($_GET['r']) && $_GET['r']== 'json'){
+         return json_encode($productos);
+      }
+      return $this->procesarVista($productos);
+  }
 
 }
 
--- Modelos/ProductoMdl.php
+++ Modelos/ProductoMdl.php
@@ -11,9 +11,56 @@
         function obtenerProductos()
         {
 
-          $st = $db->prepare('SELECT * FROM Productos');
+          $st = $db->prepare('SELECT * FROM productos');
 
           $st->execute();
+
+          $result = $st->fetchAll(PDO::FETCH_OBJ);
+
+          return $result;
+
+        }
+
+        function insertarProducto($nombre, $categoria, $descripcion, $ruta_imagen)
+        {
+
+          $st = $db->prepare('INSERT INTO productos (nombre,categoria,descripcion,ruta_imagen)
+                              VALUES (:nombre,:categoria,:descripcion,:ruta_imagen)');
+
+          $st->execute(array(':nombre' => $nombre, ':categoria' => $categoria,
+                             ':descripcion' => $descripcion, ':ruta_imagen' => $ruta_imagen));
+
+
+        }
+
+        function modificarProducto($id,$nombre, $categoria, $descripcion, $ruta_imagen)
+        {
+
+          $st = $db->prepare('UPDATE productos SET  nombre = :nombre, categoria = :categoria,
+                             descripcion = :descripcion, ruta_imagen = :ruta_imagen
+                             WHERE  idproductos = :id');
+
+          $st->execute(array(':nombre' => $nombre, ':categoria' => $categoria,
+                             ':descripcion' => $descripcion, ':ruta_imagen' => $ruta_imagen)
+                             ':id' => $id);
+
+        }
+
+        function borrarProducto($id)
+        {
+
+          $st = $db->prepare('DELETE FROM productos
+                              WHERE idproductos = :id');
+
+          $st->execute(array(':id' => $id);
+
+        }
+        function obtenerProductos_($id)
+        {
+
+          $st = $db->prepare('SELECT * FROM productos WHERE id = :id');
+
+          $st->execute(array(':id' => $id);
 
           $result = $st->fetchAll(PDO::FETCH_OBJ);
 
